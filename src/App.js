bb18126fe3d9788f53e772323420df24a32383c9
Becca Wyland <rwyland@uwalumni.com>
2021-07-28 13:23:18 -0500

    create contact component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import About from './components/About';
 import Nav from './components/Nav';
+import Contact from './components/Contact';
 
 
 function App() {
   return (
     <div>
       <Nav></Nav>
-      
+
       <main>
         <About></About>
+        <Contact></Contact>
       </main>
     </div>
   );
---
+++ src/components/Contact/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact () {
+    return (
+        <section className="my-5">
+            <h1 id="contact-me">Contact Me</h1>
+        </section>
+    );
+}
+
+export default Contact;
