f32ca33ba0480000e4d2addf766ad72df528a99d
Becca Wyland <rwyland@uwalumni.com>
2021-07-28 15:57:03 -0500

    add content to resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,28 @@ function Resume () {
         <section className="my-5">
             <h1 id="resume">Resume</h1>
 
-            <h3></h3>
+            <h2>Front End Skills</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>jQuery</li>
+                <li>React</li>
+                <li>Responsive Design</li>
+                <li>Bootstrap</li>
+                <li>Tailwind</li>
+            </ul>
+            
+            <h2>Back End Skills</h2>
+            <ul>
+                <li>APIs</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>SQL/MySQL</li>
+                <li>Mongoose/MongoDB</li>
+                <li>REST</li>
+                <li>GraphQL</li>
+            </ul>
         </section>
     );
 }
