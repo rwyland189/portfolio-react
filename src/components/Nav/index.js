abc02fe0542d4a388ee09397627602300bf441e6
Becca Wyland <rwyland@uwalumni.com>
2021-07-28 13:18:23 -0500

    create nav component

---
+++ src/components/Nav/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Nav() {
+    return (
+        <header>
+            <h2>
+                <a href="/">Becca Wyland</a>
+            </h2>
+
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <span>About Me</span>
+                    </li>
+
+                    <li classname="mx-2">
+                        <span>Contact Me</span>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Nav;
