8b5922131e33c31f3f555e40a43638a6876386b1
Becca Wyland <rwyland@uwalumni.com>
2021-08-08 17:43:38 -0500

    add bbcm link

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,6 +4,13 @@ function Portfolio () {
     return (
         <section className="my-5">
             <h1 id="portfolio">Portfolio</h1>
+
+            <a href="https://bbcm-programs.herokuapp.com/" target="_blank" className="bbcm">
+                <div>
+                    <h4>BBCM Programs</h4>
+                    <span>MERN SPA</span>
+                </div>
+            </a>
         </section>
     );
 }
