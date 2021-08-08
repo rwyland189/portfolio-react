d9275140fda9d8e710aa63cd0241c28e17187fc6
Becca Wyland <rwyland@uwalumni.com>
2021-08-08 17:46:55 -0500

    link

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,12 +5,10 @@ function Portfolio () {
         <section className="my-5">
             <h1 id="portfolio">Portfolio</h1>
 
-            <a href="https://bbcm-programs.herokuapp.com/" target="_blank" className="bbcm">
-                <div>
+            <a href="https://bbcm-programs.herokuapp.com/" target="_blank">
                     <h4>BBCM Programs</h4>
-                    <span>MERN SPA</span>
-                </div>
             </a>
+                   
         </section>
     );
 }
